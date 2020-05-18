82eff99000480d38ead308fa4346095be35a1487
rotunstall <rotunstall@gmail.com>
2020-05-18 00:23:19 -0400

    generating links dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -17,9 +17,8 @@ document.addEventListener("DOMContentLoaded", function getTeamsData() {
 			teamsData = [...res.data.teams].map((item) => {
 				buildConferences(item);
 				buildDivisions(item);
-				teamsData.push(item);
+				return item;
 			});
-			console.log(teamsData);
 		})
 		.catch((err) => console.error(err));
 });
@@ -93,104 +92,55 @@ function buildDivisions(item) {
 		item.strDivision = "Southeast";
 	}
 }
-
 console.log(teamsData);
-
 teamButton.addEventListener("click", teamButtonAction);
 
-let boston = document.querySelector("#nba134860");
-boston.addEventListener("click", locateTeamData);
-
-let brooklyn = document.querySelector("#nba134861");
-brooklyn.addEventListener("click", locateTeamData);
-
-let newYork = document.querySelector("#nba134862");
-newYork.addEventListener("click", locateTeamData);
-
-let philadelphia = document.querySelector("#nba134863");
-philadelphia.addEventListener("click", locateTeamData);
-
-let toronto = document.querySelector("#nba134864");
-toronto.addEventListener("click", locateTeamData);
-
-let chicago = document.querySelector("#nba134870");
-chicago.addEventListener("click", locateTeamData);
-
-let cleveland = document.querySelector("#nba134871");
-cleveland.addEventListener("click", locateTeamData);
-
-let detroit = document.querySelector("#nba134872");
-detroit.addEventListener("click", locateTeamData);
-
-let indiana = document.querySelector("#nba134873");
-indiana.addEventListener("click", locateTeamData);
-
-let milwaukee = document.querySelector("#nba134874");
-milwaukee.addEventListener("click", locateTeamData);
-
-let atlanta = document.querySelector("#nba134880");
-atlanta.addEventListener("click", locateTeamData);
-
-let charlotte = document.querySelector("#nba134881");
-charlotte.addEventListener("click", locateTeamData);
-
-let miami = document.querySelector("#nba134882");
-miami.addEventListener("click", locateTeamData);
-
-let orlando = document.querySelector("#nba134883");
-orlando.addEventListener("click", locateTeamData);
-
-let washington = document.querySelector("#nba134884");
-washington.addEventListener("click", locateTeamData);
-
-let denver = document.querySelector("#nba134885");
-denver.addEventListener("click", locateTeamData);
-
-let minnesota = document.querySelector("#nba134886");
-minnesota.addEventListener("click", locateTeamData);
-
-let oklahoma = document.querySelector("#nba134887");
-oklahoma.addEventListener("click", locateTeamData);
-
-let portland = document.querySelector("#nba134888");
-portland.addEventListener("click", locateTeamData);
-
-let utah = document.querySelector("#nba134889");
-utah.addEventListener("click", locateTeamData);
-
-let goldenState = document.querySelector("#nba134865");
-goldenState.addEventListener("click", locateTeamData);
-
-let losAngelesC = document.querySelector("#nba134866");
-losAngelesC.addEventListener("click", locateTeamData);
-
-let losAngelesL = document.querySelector("#nba134867");
-losAngelesL.addEventListener("click", locateTeamData);
-
-let phoenix = document.querySelector("#nba134868");
-phoenix.addEventListener("click", locateTeamData);
-
-let sacramento = document.querySelector("#nba134869");
-sacramento.addEventListener("click", locateTeamData);
-
-let dallas = document.querySelector("#nba134875");
-dallas.addEventListener("click", locateTeamData);
-
-let houston = document.querySelector("#nba134876");
-houston.addEventListener("click", locateTeamData);
-
-let memphis = document.querySelector("#nba134877");
-memphis.addEventListener("click", locateTeamData);
-
-let newOrleans = document.querySelector("#nba134878");
-newOrleans.addEventListener("click", locateTeamData);
-
-let sanAntonio = document.querySelector("#nba134879");
-sanAntonio.addEventListener("click", locateTeamData);
-
 function teamButtonAction() {
+	addTeamLinksToUi();
 	leagueInfoSection.classList.remove("d-none");
 	teamInfoSection.classList.add("d-none");
+}
+
+function buildTeamLinks() {
+	const teamLink = document.createElement("a");
+	teamLink.className = "team-name";
+	teamLink.innerHTML = `${item.strTeam}`;
+	teamLink.onclick = locateTeamData;
+
+	const newLi = document.createElement("li");
+	newLi.id = `nba${item.idTeam}`;
+	newLi.className = "team-wrapper";
+	newLi.appendChild(teamLink);
+}
+
+function addTeamLinksToUi() {
+	teamsData.forEach((elem) => {
+		if (elem.strDivision == "Northwest") {
+			buildDivisions();
+			let ul = document.querySelector(".division-northwest");
+			ul.appendChild(newLi);
+		} else if (elem.strDivision == "Pacific") {
+			buildDivisions();
+			let ul = document.querySelector(".division-pacific");
+			ul.appendChild(newLi);
+		} else if (elem.strDivision == "Southwest") {
+			buildDivisions();
+			let ul = document.querySelector(".division-southwest");
+			ul.appendChild(newLi);
+		} else if (elem.strDivision == "Atlantic") {
+			buildDivisions();
+			let ul = document.querySelector(".division-atlantic");
+			ul.appendChild(newLi);
+		} else if (elem.strDivision == "Central") {
+			buildDivisions();
+			let ul = document.querySelector(".division-central");
+			ul.appendChild(newLi);
+		} else {
+			buildDivisions();
+			let ul = document.querySelector(".division-southeast");
+			ul.appendChild(newLi);
+		}
+	});
 }
 
 function locateTeamData(e) {
@@ -198,8 +148,6 @@ function locateTeamData(e) {
 	teamIdUi = teamIdUi.substring(3);
 	console.log(teamIdUi);
 	let selectedTeam = teamsData.find((item) => item.idTeam == teamIdUi);
-	console.log(selectedTeam);
-
 	displayTeamData(selectedTeam);
 }
 
@@ -207,9 +155,12 @@ function displayTeamData(selectedTeam) {
 	let newHtml = `
     <h2 class="team-info-header text-center text-uppercase">
   ${selectedTeam.strTeam} <small>(${selectedTeam.strTeamShort})</small></h2>
-    <h4 class="team-info-subheader text-center">
+  <div class="conference-info text-center mb-1">${selectedTeam.strConference} Conference</div>
+  <div class="division-info text-center mb-3">${selectedTeam.strDivision} Division</div>
+  <h4 class="team-info-subheader text-center">      
 	Year Established:<br /> <span class="year-established">${selectedTeam.intFormedYear}</span>
-	</h4>
+  </h4>
+
 	<div class="stadium-info text-center mb-5">
 	<p class="stadium-name">Stadium Name:<br /> <strong>${selectedTeam.strStadium}</strong></p>
 	<p class="stadium-location">Location:<br /> <strong>${selectedTeam.strStadiumLocation}</strong></p>
